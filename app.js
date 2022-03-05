8dca2f6f925db01b523907972eaafefb775fff57
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-04 22:06:28 -0600

    Refactorizando código agregando un archivo yargs.js y añadiendo descripcion en node app --help

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
 
 const { demandOption } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs')
-                .options({
-                    'b': {
-                        alias: 'base',
-                        type: 'number',
-                        demandOption: true
-                    }, 
-                    'l': {
-                        alias: 'listar',
-                        type: 'boolean',
-                        default: false
-                    }
-                })
-                .check((argv, options) => {
-                    if(isNaN(argv.base)) {
-                        throw 'La base tiene que ser un número'
-                    }
-                    return true
-                })
-                .argv
-
-    
+const argv = require('./config/yargs');
 
 console.clear();
-// const base = 2;
-
-/* Argumentos de consola - Primera Forma no recomendada */
-/* const [, , arg3 = 'base=1'] = process.argv;
-console.log(arg3);
-
-const [, base = 1] = arg3.split('=');
-console.log(base); */
 
 /* Uso de argv */
-console.log(argv);
-console.log('Base dado por yargs:', argv.b);
-console.log('Listar dado por yargs:', argv.l);
-
 crearArchivo(argv.b, argv.l)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(console.log);
