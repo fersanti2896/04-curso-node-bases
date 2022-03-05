241d0b5e651b776ccba617eb7333cd571790cbca
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-04 19:01:34 -0600

    Instalando yargs para crear línea de comandos en la consola

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv
 
 console.clear();
 // const base = 2;
 
 /* Argumentos de consola - Primera Forma no recomendada */
-const [, , arg3 = 'base=1'] = process.argv;
+/* const [, , arg3 = 'base=1'] = process.argv;
 console.log(arg3);
 
 const [, base = 1] = arg3.split('=');
-console.log(base);
+console.log(base); */
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(console.log);
+/* Uso de argv */
+console.log(process.argv);
+console.log(argv);
+console.log('Base dado por yargs:', argv.base);
+
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(console.log);
