1285609224f3c65abd015675154938cc12809b7b
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-04 18:43:50 -0600

    Agregando package.json con npm init

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
-const base = 2;
+// const base = 2;
+
+/* Argumentos de consola - Primera Forma no recomendada */
+const [, , arg3 = 'base=1'] = process.argv;
+console.log(arg3);
+
+const [, base = 1] = arg3.split('=');
+console.log(base);
 
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
